b2c767f8ec4e6b02d414b72b343a592ee6efb2cd
Ashen Gamage <ashengamage223@gmail.com>
2025-06-28 03:43:34 +0530

    initializd first game codeL: - create window - rename the window -update n fixed cloud picture -quite button -Resolved indentation issue

---
+++ game/__init__.py
@@ -0,0 +1,24 @@
+import sys
+import pygame
+
+class Game:  # use object-oriented
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('VoidLine-0101')  # name for the game window
+        self.screen = pygame.display.set_mode((650, 450))  # initialize the window
+        self.clock = pygame.time.Clock()  # clock rate
+        self.img = pygame.image.load('../assets/images/Cloud.jpg')  # give location of the image
+        self.img_pos = [11, 26]
+
+    def run(self):
+        while True:
+            self.screen.blit(self.img, (100, 100))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()  # creates Game object and runs the game loop
