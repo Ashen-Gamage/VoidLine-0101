ef1965044e03ed0a56d67ec67c7c520ed0f9bd6e
Ashen Gamage <ashengamage223@gmail.com>
2025-07-05 21:41:19 +0530

    Add key press/release handling for up/down movement and implement screen refresh

--- game/__init__.py
+++ game/__init__.py
@@ -7,16 +7,30 @@ class Game:  # use object-oriented
         pygame.display.set_caption('VoidLine-0101')  # name for the game window
         self.screen = pygame.display.set_mode((650, 450))  # initialize the window
         self.clock = pygame.time.Clock()  # clock rate
-        self.img = pygame.image.load('../assets/images/Cloud.jpg')  # give location of the image
+        self.img = pygame.image.load('../assets/images/Cloud.png')  # give location of the image
         self.img_pos = [11, 26]
+        self.img_pos =[160,260]
+        self.movement=[False,False]#enable movement function
 
     def run(self):
         while True:
-            self.screen.blit(self.img, (100, 100))# put the image in specific place
+            self.screen.fill ((0,51,12))
+            self.img_pos[1] += (self.movement[1]-self.movement[0]) *5
+            self.screen.blit(self.img, self.img_pos)# put the image in specific place like on screen serface put the image  serface
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN: # check wheather key is press or not if press movement gonna work
+                    if event.key == pygame.K_UP:# If the UP arrow key is pressed
+                        self.movement [0] =True  # Set upward movement to True
+                    if event.key == pygame.K_DOWN:    # If the DOWN arrow key is pressed
+                        self.movement[1] = True # Set downward movement to True
+                if event.type == pygame.KEYUP: # Check if any key is not press
+                    if event.key == pygame.K_UP: # Check if any key is released
+                        self.movement [0]= False # Stop upward movement
+                    if event.key == pygame.K_DOWN: # If the DOWN arrow key is released
+                        self.movement [1] = False  # Stop downward movement
 
             pygame.display.update()
             self.clock.tick(60)
