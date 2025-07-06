8d08ea81d783a4913332c16adbc4cf6733aa9d43
Ashen Gamage <ashengamage223@gmail.com>
2025-07-06 18:27:46 +0530

    Implemented a rectangular collision box around the image.
    When collision is detected, the rectangle lights up to indicate impact.
    
    Helps visually debug and enhance interaction in the game.

--- game/__init__.py
+++ game/__init__.py
@@ -8,15 +8,26 @@ class Game:  # use object-oriented
         self.screen = pygame.display.set_mode((650, 450))  # initialize the window
         self.clock = pygame.time.Clock()  # clock rate
         self.img = pygame.image.load('../assets/images/Cloud.png')  # give location of the image
+        self.img.set_colorkey((255,255,255))# remove background from picture its like chromo key in video editing
         self.img_pos = [11, 26]
         self.img_pos =[160,260]
         self.movement=[False,False]#enable movement function
 
+        self.collision_area = pygame.Rect(50,50,300,50)# open collision rectangle
+
     def run(self):
         while True:
             self.screen.fill ((0,51,12))
+
+            img_r = pygame.Rect(self.img_pos[0],self.img_pos[1],self.img.get_width(),self.img.get_height())# react to collision box , first two is the position second two is parameters for rectangle
+            if img_r.colliderect(self.collision_area):#Now its collision test
+                pygame.draw.rect(self.screen,(00,100,255),self.collision_area)
+            else:
+                pygame.draw.rect(self.screen,(00,50,105),self.collision_area)
+
             self.img_pos[1] += (self.movement[1]-self.movement[0]) *5
             self.screen.blit(self.img, self.img_pos)# put the image in specific place like on screen serface put the image  serface
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
